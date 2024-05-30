6ad8d5b1129bc03df08638295ba3e01c5f935870
naden mohamed <nadenms783@gmail.com>
2024-05-30 20:45:03 +0300

    Create a view Controller to connect UI with thier handlers

--- ShopifyApp/CustomShoppingCartCellTableViewCell.swift
+++ ShopifyApp/CustomShoppingCartCellTableViewCell.swift
@@ -16,7 +16,7 @@ class CustomShoppingCartCellTableViewCell: UITableViewCell {
     @IBOutlet weak var productDescription: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
