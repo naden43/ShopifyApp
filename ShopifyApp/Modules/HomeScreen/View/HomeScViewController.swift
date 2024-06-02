586e8062f9808a85f0c5e15d5cfef9befcb839d5
Aya-moustafa <aiyamoustafa910@gmail.com>
2024-06-02 14:06:12 +0300

    add product nib file

---
+++ ShopifyApp/Modules/HomeScreen/View/HomeScViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeScViewController.swift
+//  ShopifyApp
+//
+//  Created by Aya Mostafa on 02/06/2024.
+//
+
+import UIKit
+
+class HomeScViewController: UIViewController {
+
+    @IBOutlet weak var brandsCollection: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
